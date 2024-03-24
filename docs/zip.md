eff3740d605bbd0125531d7680674234c6228087
Bruce Li <isanoob1897@outlook.com>
2024-03-24 12:17:35 -0700

    add

---
+++ docs/zip.md
@@ -0,0 +1,5 @@
+# Zip
+The zip utility allows you to zip files and extract `.zip` files.
+Usage:
+
+    
--- src/tools.py
+++ src/tools.py
@@ -1,13 +1,12 @@
 
 def editor(file):
         # Simple Text editor
-        with open(file, "a") as files:
-            for line in files:
-                print(line)
-            while True:
-                line = input()
-                if line.startswith(":q"):
-                    break
-                else:
-                    file.write(line+'\n')
-editor("test.py")
+        with open(file, "r+") as files:
+                for line in files:
+                    print(line)
+                while True:
+                    line = input()
+                    if line.startswith(":q"):
+                        break
+                    else:
+                        files.write(line+'\n')
